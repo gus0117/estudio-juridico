0822d6cc989186d17a2bf4b9b1057708ebacfb33
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-02-10 21:03:48 -0300

    Se quito el sweet alert de index y logica de sweet alert de js

--- js/main.js
+++ js/main.js
@@ -4,22 +4,3 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
-
-/* // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the navbar
-const navbar = document.getElementsByClassName('navbar')[0];
-
-const sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
-    } else {
-      navbar.classList.remove("sticky");
-      console.log("Sticky removed")
-    }
-  } */
-
